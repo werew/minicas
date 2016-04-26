1289208b3e5491a0ffb05b7ae0d50f216cdf9e56
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2016-04-26 13:16:55 +0200

    Main reading loop

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "parser.h"
+
+//int main(int argc, char **argv) { //TODO add -f option
+int main(void){
+   struct stat buf;
+   
+   FILE *f_in = fdopen(0, "r");
+   char *line;
+   size_t n=0;
+
+   if (!fstat(0, &buf) && S_ISREG(buf.st_mode)) {
+
+       line=NULL;
+       while (getline(&line, &n, f_in) != -1) {
+	   eval_input(line);
+           free(line); line=NULL;
+        }
+
+    } else {
+
+       printf("prompt> ");
+       line=NULL;
+       while (getline(&line, &n, f_in) != -1) {
+	   eval_input(line);
+           printf("prompt> ");
+           free(line); line=NULL;
+        }
+    }
+       
+   return 0;   
+}
