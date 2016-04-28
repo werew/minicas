46e1a6f174b6633cb4c310b2ae0e4c7beef6f8a5
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2016-04-28 18:41:06 +0200

    Complete Cmd API definition

--- include/cmd.h
+++ include/cmd.h
@@ -22,6 +22,9 @@ typedef struct {
 
 /* Create/destroy command */
 Cmd new_cmd(Ref (*fun) (ref_list), int n_args, ref_t** types);
-
+void drop_cmd(Cmd c);
+Ref new_cref(char* name,Ref (*fun) (ref_list), int n_args, ref_t** types);
+Ref set_cref(char* name,Ref (*fun) (ref_list), int n_args, ref_t** types);
+Ref get_cmd(const char* name);
 
 #endif
--- src/cmd.c
+++ src/cmd.c
@@ -7,7 +7,8 @@
 /**
  * Creates a new command structure
  * @param fun Pointer to the handler
- * @param args List of predefined arguments
+ * @param n_args Minimum number of arguments of the given program
+ * @param types Array representing the types of the arguments
  * @return The newly crafted command structure or NULL in case of error
  */
 Cmd new_cmd(Ref (*fun) (ref_list), int n_args, ref_t** types){
@@ -21,3 +22,75 @@ Cmd new_cmd(Ref (*fun) (ref_list), int n_args, ref_t** types){
 
 	return c;
 }
+
+/**
+ * Drop a stored Cmd structure
+ * @param c Cmd to drop
+ */
+void drop_cmd(Cmd c){
+	free(c);
+}
+
+/**
+ * Creates a command and a reference pointing to it
+ * @param name Name of the reference
+ * @param fun Pointer to the handler
+ * @param n_args Minimum number of arguments of the given program
+ * @param types Array representing the types of the arguments
+ * @return A pointer to the new reference or NULL in case 
+ *	   of error
+ */
+Ref new_cref(char* name,Ref (*fun) (ref_list), int n_args, ref_t** types){
+	
+	Cmd c = new_cmd(fun, n_args, types);
+	if (c == NULL) return NULL;
+
+	Ref r = new_ref(name, c, CMD);
+	if (r == NULL){
+		drop_cmd(c);
+		return NULL;
+	}
+	
+	return r;
+}	
+
+
+/**
+ * Create and store or update a command
+ * @param name Name of the command to store/update
+ * @param fun Pointer to the handler
+ * @param n_args Minimum number of arguments of the given program
+ * @param types Array representing the types of the arguments
+ * @return The reference of the newly stored/updated command,
+ *	   or NULL in case of error
+ */
+Ref set_cref(char* name,Ref (*fun) (ref_list), int n_args, ref_t** types){
+
+	Cmd c = new_cmd(fun, n_args, types);
+	if (c == NULL) return NULL;
+
+	Ref r = set_ref(name, c, CMD);
+	if (r == NULL) {
+		drop_cmd(c);
+		return NULL;
+	}
+	
+	return r;
+
+}
+
+/**
+ * Find and get a stored command
+ * @param name The name of the command to get
+ * @return The reference or NULL if any command having the
+ *	   given name has been found
+ */
+Ref get_cmd(const char* name){
+	Ref r = get_ref(name, CMD);
+	if (r == NULL) return NULL;
+
+	return r;
+}
+
+
+
