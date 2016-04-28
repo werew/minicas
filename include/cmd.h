863207018e394a6f5b6f2ab6d9890ed397eba5ab
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2016-04-28 16:30:44 +0200

    New type command

--- include/cmd.h
+++ include/cmd.h
@@ -4,20 +4,11 @@
 #include "xtypes.h"
 #include "ref.h"
 
-typedef struct argt {
-	ref_t type;
-	struct argt* sub;
-	} arg_t;
-
 /* Command */
 typedef struct {
 	Ref (*fun) (ref_list);
-	int n_args;
-	arg_t type[];
+	ref_t type[];
 	} s_cmd, *Cmd;
-
-
-
 
 
 
