93b916006059cd2d4ccf49c5c122eadd21b3b53d
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2016-04-29 23:55:05 +0200

    Add call to mult

--- src/mod_matrix.c
+++ src/mod_matrix.c
@@ -18,6 +18,16 @@ void load_mod_matrix(void){
 	load = set_fun("transpose",transpose_call,NULL);
 	if (load == NULL) inst_err(ELOAD, "function transpose");
 	
+	load = set_fun("mult",mult_call,NULL);
+	if (load == NULL) inst_err(ELOAD, "function mult");
+}
+
+/* Check if reference is a matrix */
+bool expect_Matrix(Ref r){
+	if (v_isa(r, MATRIX) == true) return true;
+	
+	set_err(ETYPE, "argument was expected to be a Matrix");
+	return false;
 }
 
 /**
@@ -57,6 +67,7 @@ Ref matrix_call(ref_list args){
 	return r;
 }
 
+
 Ref transpose_call(ref_list args){
 	
 	if (args->length != 1){
@@ -86,4 +97,48 @@ Ref transpose_call(ref_list args){
 		
 } 
 			
+
+
+
+/**
+ * Multiplies all the given matrices consecutively
+ */	
+Ref mult_call(ref_list args){
 	
+	if (expect_Matrix(args->list[0]) == false) return NULL;
+		
+	Var var = (Var) args->list[0]->inst;
+	Matrix m = (Matrix) var->val;
+
+	Matrix mult = copyMatrix(m);
+	if (mult == NULL) return NULL;
+
+	unsigned int i;
+	for (i = 1; i < args->length; i++){
+		
+		
+		if (expect_Matrix(args->list[i]) == false) goto error;
+
+		var = (Var) args->list[i]->inst;
+		m = (Matrix) var->val;
+		if (m->nrows != mult->ncols) {
+			set_err(EMXDIM,"dimensions don't match");
+			goto error;
+		}
+
+		Matrix tmp = multiplication(mult, m);
+		if (tmp == NULL ) goto error;
+
+		free(mult); //FIXME
+		mult = tmp;
+	}
+    
+	/* Return a reference */
+	Ref r = new_vref(NULL, mult, MATRIX);
+	if (r == NULL) free(mult); //TODO FIX
+
+	return r;
+error:
+	free(mult); //TODO
+	return NULL;
+}
